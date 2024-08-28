e7f7bd3cf6d79acdab5af62bf911abd1d86b4a90
yangliudesigns <yangliudesigns@gmail.com>
2024-08-28 01:13:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+import dotenv from "dotenv";
 import express from "express";
+import videoRoutes from "./routes/video.js";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('BrainFlix API is running');
+app.use("/videos", videoRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Hello there!!!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// Start Express server on port 8080
+app.listen(8080, () => {
+    console.log('Server Started on http://localhost:8080');
+    console.log('Press CTRL + C to stop server');
 });
